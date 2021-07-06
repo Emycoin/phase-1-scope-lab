514b370ec410e6912edd294e8351bb531b8e2b5f
Emycoin <emytausevich@gmail.com>
2021-07-06 19:38:28 -0400

    commit tree 9be59b82faf8a2cad59a3f795eb7a65f2b619cfa

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+function upperCaseCustomerName(customerName){
+    
+    return customerName.toUppercase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    var bestCustomer = 'Sue'
+}
+const leastFavoriteCustomer = "me"
+
+function changeLeastFavoriteCustom(leastFavoriteCustomer){
+    leastFavoriteCustomer = "emily"
+}
